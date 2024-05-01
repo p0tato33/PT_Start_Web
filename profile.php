c61e28f82a554b831efd5158bce0610f0c0f39fb
bogdan <7kolesnikbogda@gmail.com>
2024-05-01 16:06:15 -0400

    abc

--- profile.php
+++ profile.php
@@ -62,6 +62,39 @@
             </div>
         </div>
     </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="hello">
+                    Привет, <?php echo $_COOKIE['User']; ?>
+                </h1>
+            </div>
+            <dib class="col-12">
+                <form method="POST" action="profile.php">
+                    <input type="text" class="form" type="text" name="title" placeholder="Заголовок вашего поста">
+                    <textarea name="text" cols="30" rows="10" placeholder="Введите текст вашего поста ..."></textarea>
+                    <button type="submit" class="btn_red" name="submit">Сохранить пост!</button>
+                </form>
+<           </div>
+        </div>
+    </div>
     <script type="text/javascript" src="js/button.js"></script>
 </body>
 </html>
+
+<?php
+require_once('db.php');
+
+$link = mysqli_connect('127.0.0.1', 'root', 'kali', 'first_db');
+
+if (isset($_POST['submit'])) {
+    $title = $_POST['title'];
+    $main_text = $_POST['text'];
+
+    if (!$title || !$main_text) die ("Заполните все поля");
+
+    $sql = "INSERT INTO posts (title, main_text) VALUES ('$title', '$main_text')";
+
+    if (!mysqli_query($link, $sql)) die("Не удалось добавить пост");
+}
+?>
